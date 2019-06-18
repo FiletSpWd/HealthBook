b30fee36439afc52254d39fbc879f765eb6695a6
FiletSpWd <steppe.winds@gmail.com>
2019-06-18 22:23:16 +0300

    37 Fix menu model and views

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,7 +3,7 @@ class Menu < ApplicationRecord
   belongs_to :user
 
   def get_calories
-    calories = self.weight * self.recipe.get_params('calories').to_f / 100
+    calories = self.weight * self.recipe.get_params('calories').to_f
     sprintf('%.2f', calories)
   end
 
